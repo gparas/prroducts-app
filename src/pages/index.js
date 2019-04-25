a3dbaf1c1f25db10e009267062ec248e8a64b38d
gparas <parageos@gmail.com>
2019-04-25 23:08:21 +0300

    fix warning

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql } from 'gatsby';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
